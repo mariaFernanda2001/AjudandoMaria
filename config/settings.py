1d137022ada4975dba766645f5bce1c36fbf8551
mouraCorazim <lucasmouracorazim1@gmail.com>
2019-10-04 12:02:01 -0300

    Tá com problema ainda

--- config/settings.py
+++ config/settings.py
@@ -123,8 +123,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = "{}/media".format(BASE_DIR)
+# MEDIA_ROOT = "{}/media".format(BASE_DIR)
 
 django_heroku.settings(locals())
